7a11aaa9905ab40a86ccd9da47a03ed4493f1f76
I_Lea51 <miss.ivasisi@gmail.com>
2023-05-03 19:06:01 +0300

    4_hw

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,10 +3,21 @@
 function parseProducts(json) {
     // Аргументом функции является JSON
 
+    let data = JSON.parse(json);
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+    return data.products;
+}
+
+function renderProductsCards(json) {
+    clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+    for (i = 0; i<=(length-1);i+=1){
+        addProduct(products[i]);
+    }
 }
 
 // Напишите функцию renderProductsCards(json)
